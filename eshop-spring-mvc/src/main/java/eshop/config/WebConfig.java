ee64d6c612bba5cd4d065994e8c9376c2c9b11c1
Eric SULTAN <e.sultan@ajc-ingenierie.fr>
2025-04-10 12:17:25 +0200

    Spring MVC - Jour 1 - Spring MVC Début

---
+++ eshop-spring-mvc/src/main/java/eshop/config/WebConfig.java
@@ -0,0 +1,33 @@
+package eshop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("eshop.controller")
+public class WebConfig implements WebMvcConfigurer {
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+		registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+	}
+
+	// Configuration du mode restitution des Vues avec les JSP/JSTL
+	@Bean
+	public UrlBasedViewResolver viewResolver() {
+		UrlBasedViewResolver viewResolver = new UrlBasedViewResolver();
+
+		viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+}
---
+++ eshop-spring-mvc/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,29 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="jakarta.tags.core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Accueil</title>
+<link rel="stylesheet"
+	href="<c:url value="/css/bootstrap.min.css"/>">
+<link rel="stylesheet"
+	href="<c:url value="/css/bootstrap-icons.min.css"/>">
+</head>
+<body>
+
+
+	<div class="container mt-3">
+	  <h2>Ma page d'accueil</h2>
+	  <div class="mt-4 p-5 bg-primary text-white rounded">
+	    <h1>Bonjour à tous</h1> 
+
+	  </div>
+	</div>
+
+
+
+	<script src="<c:url value="/js/bootstrap.bundle.min.js"/>"></script>
+</body>
+</html>
