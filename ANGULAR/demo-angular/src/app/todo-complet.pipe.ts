9d7322993872386cfea7ff49e6f1272288979a90
Jérémy <jeremy.perrouault@ascadis.fr>
2025-04-23 08:15:39 +0000

    ANGULAR - JOUR 2 - CORRECTION PIPE TODO

--- ANGULAR/demo-angular/src/app/todo-complet.pipe.ts
+++ ANGULAR/demo-angular/src/app/todo-complet.pipe.ts
@@ -8,7 +8,20 @@ export class TodoCompletPipe implements PipeTransform {
   transform(value: unknown, ...args: unknown[]): unknown {
     console.log("valeur ici", value);
     console.log("args ici", args);
-    
-    return "Démonstration";
+
+    // Vérifier le contenu de args
+    // SI le contenu de args est "couleur", alors on retournera une couleur
+    // Couleur rouge hsl(341 79% 53%)
+    // Couleur verte hsl(153 48% 49%)
+    // SINON, on retourne le texte terminé ou pas terminé
+
+    // Avec la couleur, modifier le style couleur de la ligne ou la colonne
+
+
+    if (value) {
+      return "Terminé !";
+    }
+
+    return "Non terminé";
   }
 }
