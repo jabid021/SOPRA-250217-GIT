089b70b344268a66065691713eb178ca531cf470
Jérémy <jeremy.perrouault@ascadis.fr>
2025-04-24 08:38:48 +0000

    ANGULAR - JOUR 3 - INTRO OBSERVABLE SUBJECT

--- ANGULAR/demo-angular/src/app/observable/observable.component.ts
+++ ANGULAR/demo-angular/src/app/observable/observable.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, scan, Subscription } from 'rxjs';
+import { count, Observable, scan, Subject, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-observable',
@@ -31,16 +31,27 @@ export class ObservableComponent implements OnInit, OnDestroy {
       this.counters.push(val);
     });
 
-    this.arrayCounterObs$ = new Observable<number>(obs => {
-      setInterval(() => {
-        obs.next(counter);
-      }, 1_000);
-    }).pipe(
-      // C'est comme un reduce, mais en temps réel (reduce qui fera tout à la fin)
-      // Dans le scan, on a une fonction lambda qui accumule
-      // > Et la valeur initiale
+    // this.arrayCounterObs$ = new Observable<number>(obs => {
+    //   setInterval(() => {
+    //     obs.next(counter);
+    //   }, 1_000);
+    // }).pipe(
+    //   // C'est comme un reduce, mais en temps réel (reduce qui fera tout à la fin)
+    //   // Dans le scan, on a une fonction lambda qui accumule
+    //   // > Et la valeur initiale
+    //   scan((accumulateur: number[], valeur: number) => [ ...accumulateur, valeur ], [])
+    // );
+
+    // Le Subject nous permettra d'alimenter le flux en de hors de la déclaration de l'Observable
+    const arrayCounterSubject = new Subject<number>();
+
+    this.arrayCounterObs$ = arrayCounterSubject.pipe(
       scan((accumulateur: number[], valeur: number) => [ ...accumulateur, valeur ], [])
     );
+
+    setInterval(() => {
+      arrayCounterSubject.next(counter);
+    }, 1_000);
   }
 
   ngOnDestroy(): void {
