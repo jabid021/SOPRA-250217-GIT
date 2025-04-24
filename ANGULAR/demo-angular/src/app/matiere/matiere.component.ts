c9763fbe685525cbed9a3ee48fe2a13e51ea4812
Jérémy <jeremy.perrouault@ascadis.fr>
2025-04-24 11:32:02 +0000

    ANGULAR - JOUR 3 - HTTPCLIENT MATIERES - GET

--- ANGULAR/demo-angular/src/app/matiere/matiere.component.ts
+++ ANGULAR/demo-angular/src/app/matiere/matiere.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Matiere } from '../matiere';
 import { MatiereService } from '../matiere.service';
@@ -9,12 +9,10 @@ import { MatiereService } from '../matiere.service';
   templateUrl: './matiere.component.html',
   styleUrl: './matiere.component.css'
 })
-export class MatiereComponent implements OnInit {
+export class MatiereComponent {
   public matieres$!: Observable<Matiere[]>;
 
-  constructor(private service: MatiereService) { }
-  
-  ngOnInit(): void {
+  constructor(private service: MatiereService) {
     this.matieres$ = this.service.findAll();
   }
 }
