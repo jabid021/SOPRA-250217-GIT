fb636867703e9754149ce23d27a0c0d087e855c1
Jérémy <jeremy.perrouault@ascadis.fr>
2025-04-24 12:41:40 +0000

    ANGULAR - JOUR 3 - HTTPCLIENT MATIERE AVEC JWT

--- ANGULAR/demo-angular/src/app/matiere.service.ts
+++ ANGULAR/demo-angular/src/app/matiere.service.ts
@@ -10,6 +10,10 @@ export class MatiereService {
   constructor(private http: HttpClient) { }
 
   public findAll(): Observable<Matiere[]> {
-    return this.http.get<Matiere[]>("http://localhost:8080/api/matiere");
-  }  
+    return this.http.get<Matiere[]>("http://localhost:8080/api/matiere", {
+      headers: {
+        'Authorization': "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiaWF0IjoxNzQ1NDk4MjQ0LCJleHAiOjE3NDU1MDE4NDR9.Z59OkTHV204DT4W3LE_IV76a0wYut_lDsL7aQsAlH-6x_qnM_lGZWgO8jJOoifiOJjqkmTVMTsGEnnMMftZfIA"
+      }
+    });
+  }
 }
