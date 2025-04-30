f6779cffdb4c224302f5dc335453ce8fada9d851
Jérémy <jeremy.perrouault@ascadis.fr>
2025-04-30 14:20:30 +0000

    DOCKER - LISTE COMMANDES - JOUR 1

---
+++ DOCKER/commandes/images.sh
@@ -0,0 +1,10 @@
+# Voir la liste des images présentes
+docker image ls
+docker images
+
+# Supprimer une image
+docker image rm nom_image:tag
+docker image rm id_image
+
+# Récupérer une image du Docker Hub
+document pull image:tag
