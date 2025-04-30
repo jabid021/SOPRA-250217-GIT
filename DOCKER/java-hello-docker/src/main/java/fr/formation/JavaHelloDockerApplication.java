b9e59f35d80eb520b478861382fd6aef21fffae0
Jérémy <jeremy.perrouault@ascadis.fr>
2025-04-30 10:50:10 +0000

    DOCKER - INIT PROJET JAVA - JOUR 1

---
+++ DOCKER/java-hello-docker/src/main/java/fr/formation/JavaHelloDockerApplication.java
@@ -0,0 +1,11 @@
+package fr.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHelloDockerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHelloDockerApplication.class, args);
+	}
+}
