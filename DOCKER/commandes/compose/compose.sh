9b5cf1257ec9fc68cd1ed80c9c7587a25a6930e6
Jérémy <jeremy.perrouault@ascadis.fr>
2025-05-05 11:10:15 +0000

    DOCKER - COMPOSE - JOUR 3

---
+++ DOCKER/commandes/compose/compose.sh
@@ -0,0 +1,8 @@
+# Pour lancer un Docker Compose (dans le répertoire où se trouve le fichier docker-compose.yml)
+docker compose up
+
+# Même chose en tâche de fond
+docker compose up -d
+
+# Pour arrêter un Docker Compose (dans le répertoire où se trouve le fichier docker-compose.yml)
+docker compose down
